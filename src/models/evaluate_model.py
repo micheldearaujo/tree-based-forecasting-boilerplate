adbe5b3a81c28702162ace6781e4cef18e513e1f
Michel <michelarrudala@gmail.com>
2024-06-12 08:09:28 -0300

    Refactored the outer pipeline function

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -6,33 +6,35 @@ import os
 import yaml
 import datetime as dt
 import logging
+import logging.config
 import argparse
+from typing import Any
 
 import pandas as pd
 import numpy as np
-
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, mean_absolute_error
 
+from src.models.train_model import train_model
 from src.visualization.data_viz import visualize_validation_results
+from src.utils import weekend_adj_forecast_horizon
 
 with open("src/configuration/logging_config.yaml", 'r') as f:  
     logging_config = yaml.safe_load(f.read())
     logging.config.dictConfig(logging_config)
     logger = logging.getLogger(__name__)
+    logging.getLogger('matplotlib').setLevel(logging.ERROR)
+    
 
 with open("src/configuration/project_config.yaml", 'r') as f:  
     config = yaml.safe_load(f.read())
     PROCESSED_DATA_PATH = config['paths']['processed_data_path']
     OUTPUT_DATA_PATH = config['paths']['output_data_path']
 
-
 def evaluate_and_store_performance(model_type, ticker_symbol, y_true, y_pred, latest_price_date, latest_run_date):
     """Evaluates model performance for a single ticker symbol on a single day and stores results."""
     logger.debug(f"Actual value: {y_true}. Predicted Value: {y_pred}")
 
-    # Calculate metrics
     bias = ((y_pred - y_true) / (y_pred + y_true)).round(3)
-
     y_true = np.array([y_true])
     y_pred = np.array([y_pred])
     mae = mean_absolute_error(y_true, y_pred).round(3)
@@ -90,7 +92,7 @@ def daily_model_evaluation(model_type=None, ticker_symbol=None):
         raise ValueError(f"Invalid model_type: {model_type}. Choose from: {available_models}")
     
     elif model_type:
-        available_models = [model_type]
+        available_models = [model_type.upper()]
 
     for ticker_symbol in historical_forecasts_df["STOCK"].unique():
         ticker_hist_forecast = historical_forecasts_df[historical_forecasts_df["STOCK"] == ticker_symbol]
@@ -115,7 +117,7 @@ def daily_model_evaluation(model_type=None, ticker_symbol=None):
             evaluate_and_store_performance(model_type, ticker_symbol, latest_value, predicted_value, latest_price_date, latest_run_date)
 
 
-def cross_validation(X: pd.DataFrame, y: pd.Series, forecast_horizon: int, stock_name: str, tunning: bool = False) -> pd.DataFrame:
+def walk_forward_validation(X: pd.DataFrame, y: pd.Series, forecast_horizon: int, model_type: Any, ticker_symbol: str, tune_params: bool = False) -> pd.DataFrame:
     """
     Make predictions for the past `forecast_horizon` days using a XGBoost model.
     This model is validated using One Shot Training, it means that we train the model
@@ -129,43 +131,37 @@ def cross_validation(X: pd.DataFrame, y: pd.Series, forecast_horizon: int, stock
     Returns:
         pred_df: Pandas DataFrame with the forecasted values
     """
+    # TODO: Continuar com a refatoração desse bloco interno
 
     # Create empty list for storing each prediction
     predictions = []
     actuals = []
     dates = []
     X_testing_df = pd.DataFrame()
+
+    forecast_horizon = weekend_adj_forecast_horizon(forecast_horizon, 2)
     
     # get the one-shot training set
-    X_train = X.iloc[:-forecast_horizon+2, :]
-    y_train = y.iloc[:-forecast_horizon+2]
+    X_train = X.iloc[:-forecast_horizon, :]
+    y_train = y.iloc[:-forecast_horizon]
+
+    print(f"Last training date: {X_train["DATE"].max()}")
 
     final_y = y_train.copy()
 
-
-    best_model = xgb.XGBRegressor(
-            eval_metric=["rmse", "logloss"],
+    best_model = train_model(
+        X_train.drop(columns=["DATE"]),
+        y_train,
+        model_type,
+        ticker_symbol,
+        tune_params,
+        save_model=False
     )
-
-    if tunning:
-        logger.debug("HyperTunning the model...")
-
-        best_model, results = tune_model_hyperparameters(best_model, X_train.drop(columns=["Date"]), y_train, param_distributions_dict[str(type(best_model)).split('.')[-1][:-2]], cv=5)
-
-    else:
-
-        logger.debug("Fitting the model without HyperTunning...")
-        best_model.fit(
-            X_train.drop("Date", axis=1),
-            y_train,
-            eval_set=[(X_train.drop("Date", axis=1), y_train)],
-            verbose=10
-        )
 
     # Iterate over the dataset to perform predictions over the forecast horizon, one by one.
     # After forecasting the next step, we need to update the "lag" features with the last forecasted
     # value
-    for day in range(forecast_horizon-2, 0, -1):
+    for day in range(forecast_horizon, 0, -1):
         
         if day != 1:
             # the testing set will be the next day after the training and we use the complete dataset
@@ -206,32 +202,35 @@ def cross_validation(X: pd.DataFrame, y: pd.Series, forecast_horizon: int, stock
             pass
 
         # make prediction
-        prediction = best_model.predict(X_test.drop("Date", axis=1))
+        print(f"Testing Date: {X_test["DATE"].min()}")
+        prediction = best_model.predict(X_test.drop("DATE", axis=1))
         final_y = pd.concat([final_y, pd.Series(prediction[0])], axis=0)
         final_y = final_y.reset_index(drop=True)
 
         # store the results
         predictions.append(prediction[0])
         actuals.append(y_test.values[0])
-        dates.append(X_test["Date"].max())
+        dates.append(X_test["DATE"].max())
 
-    pred_df = pd.DataFrame(list(zip(dates, actuals, predictions)), columns=["Date", 'Actual', 'Forecast'])
-    pred_df["Forecast"] = pred_df["Forecast"].astype("float64")
+    pred_df = pd.DataFrame(list(zip(dates, actuals, predictions)), columns=["DATE", "ACTUAL", "FORECAST"])
+    print(pred_df)
+    pred_df["FORECAST"] = pred_df["FORECAST"].astype("float64")
 
     logger.debug("Calculating the evaluation metrics...")
     model_mape = round(mean_absolute_percentage_error(actuals, predictions), 4)
     model_rmse = round(np.sqrt(mean_squared_error(actuals, predictions)), 2)
     model_mae = round(mean_absolute_error(actuals, predictions), 2)
-    model_wape = round((pred_df.Actual - pred_df.Forecast).abs().sum() / pred_df.Actual.sum(), 2)
+    model_wape = round((pred_df.ACTUAL - pred_df.FORECAST).abs().sum() / pred_df.ACTUAL.sum(), 2)
 
     pred_df["MAPE"] = model_mape
     pred_df["MAE"] = model_mae
     pred_df["WAPE"] = model_wape
     pred_df["RMSE"] = model_rmse
-    pred_df["Model"] = str(type(best_model)).split('.')[-1][:-2]
+    pred_df["MODEL"] = str(type(best_model)).split('.')[-1][:-2]
+
 
     # Plotting the Validation Results
-    validation_metrics_fig = visualize_validation_results(pred_df, model_mape, model_mae, model_wape, stock_name)
+    validation_metrics_fig = visualize_validation_results(pred_df, model_mape, model_mae, model_wape, ticker_symbol)
 
     # Plotting the Learning Results
     #learning_curves_fig, feat_imp = extract_learning_curves(best_model, display=True)
@@ -239,36 +238,54 @@ def cross_validation(X: pd.DataFrame, y: pd.Series, forecast_horizon: int, stock
     return pred_df, X_testing_df
 
 
-def model_evaluation_pipeline(tunning=False):
+def model_crossval_pipeline(tune_params, model_type, ticker_symbol):
 
-    logger.debug("Loading the featurized dataset..")
-    stock_df_feat_all = pd.read_csv(os.path.join(PROCESSED_DATA_PATH, 'processed_stock_prices.csv'), parse_dates=["Date"])
+    TARGET_NAME = config["model_config"]["TARGET_NAME"]
+    PREDICTED_NAME = config["model_config"]["PREDICTED_NAME"]
+    FORECAST_HORIZON = config['model_config']['forecast_horizon']
+    available_models = config['model_config']['available_models']
 
-    # iterate over the stocks
     validation_report_df = pd.DataFrame()
 
-    for stock_name in stock_df_feat_all["Stock"].unique():
 
-        logger.info("Testing the model for the stock: %s..."%stock_name)
-        stock_df_feat = stock_df_feat_all[stock_df_feat_all["Stock"] == stock_name].copy().drop("Stock", axis=1)
+    logger.info("Loading the featurized dataset..")
+    stock_df_feat_all = pd.read_csv(os.path.join(PROCESSED_DATA_PATH, 'processed_stock_prices.csv'), parse_dates=["DATE"])
+
+    # Check the ticker_symbol parameter
+    if ticker_symbol:
+        ticker_symbol = ticker_symbol.upper() + '.SA'
+        stock_df_feat_all = stock_df_feat_all[stock_df_feat_all["STOCK"] == ticker_symbol]
         
-        predictions_df, X_testing_df = test_model_one_shot(
-            X=stock_df_feat.drop([model_config["TARGET_NAME"]], axis=1),
-            y=stock_df_feat[model_config["TARGET_NAME"]],
-            forecast_horizon=model_config['FORECAST_HORIZON'],
-            stock_name=stock_name,
-            tunning=tunning
-        )
-
-        predictions_df["Stock"] = stock_name
-        predictions_df["Training_Date"] = dt.datetime.today().date()
-
-        validation_report_df = pd.concat([validation_report_df, predictions_df], axis=0)
+    # Check the model_type parameter 
+    if model_type is not None and model_type not in available_models:
+        raise ValueError(f"Invalid model_type: {model_type}. Choose from: {available_models}")
     
-    logger.debug("Writing the testing results dataframe...")
-    validation_report_df = validation_report_df.rename(columns={"Forecast": "Price"})
-    validation_report_df["Class"] = "Testing"
+    elif model_type:
+        available_models = [model_type.upper()]
 
+    for ticker_symbol in stock_df_feat_all["STOCK"].unique():
+        stock_df_feat = stock_df_feat_all[stock_df_feat_all["STOCK"] == ticker_symbol].copy().drop("STOCK", axis=1)
+        
+        for model_type in available_models:
+            logger.info(f"Performing model cross validation for ticker symbol [{ticker_symbol}] using model [{model_type}]...")
+ 
+            predictions_df, X_testing_df = walk_forward_validation(
+                X=stock_df_feat.drop(columns=[TARGET_NAME], axis=1),
+                y=stock_df_feat[TARGET_NAME],
+                forecast_horizon=FORECAST_HORIZON,
+                model_type=model_type,
+                ticker_symbol=ticker_symbol,
+                tune_params=tune_params
+            )
+
+            predictions_df["STOCK"] = ticker_symbol
+            predictions_df["TRAINING_DATE"] = dt.datetime.today().date()
+
+            validation_report_df = pd.concat([validation_report_df, predictions_df], axis=0)
+    
+    logger.info("Writing the testing results dataframe...")
+    # validation_report_df = validation_report_df.rename(columns={"FORECAST": "Price"})
+    validation_report_df["CLASS"] = "Testing"
     validation_report_df.to_csv(os.path.join(OUTPUT_DATA_PATH, 'validation_stock_prices.csv'), index=False)
 
 
@@ -291,5 +308,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     logger.info("Starting the Daily Model Evaluation pipeline...")
-    daily_model_evaluation(args.model_type, args.ticker_symbol)
+    # daily_model_evaluation(args.model_type, args.ticker_symbol)
+    model_crossval_pipeline(False, args.model_type.upper(), args.ticker_symbol)
     logger.info("Daily Model Evaluation Pipeline completed successfully!")
