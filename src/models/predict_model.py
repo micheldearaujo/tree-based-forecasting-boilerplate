8f5a80ef152582b86ee0bd9639529d99a56c2ec7
Michel <michelarrudala@gmail.com>
2024-06-14 13:09:37 -0300

    parametrized the predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -2,8 +2,6 @@ import sys
 import os
 sys.path.insert(0,'.')
 
-import re
-import warnings
 import yaml
 import argparse
 import logging
@@ -26,14 +24,28 @@ with open("src/configuration/logging_config.yaml", 'r') as f:
 
 with open("src/configuration/project_config.yaml", 'r') as f:  
     config = yaml.safe_load(f.read())
+    model_config = config['model_config']
+    data_config = config['data_config']
+    PROCESSED_DATA_PATH = data_config['paths']['processed_data_path']
+    PROCESSED_DATA_NAME = data_config['table_names']['processed_table_name']
+    OUTPUT_DATA_PATH = data_config['paths']['output_data_path']
+    OUTPUT_DATA_NAME = data_config['table_names']['output_table_name']
+    MODELS_PATH = data_config['paths']['models_path']
+    TARGET_COL = model_config['target_col']
+    CATEGORY_COL = model_config['category_col']
+    PREDICTED_COL = model_config['predicted_col']
+    FORECAST_HORIZON = model_config['forecast_horizon']
+    features_list = config['features_list']
+    available_models = model_config['available_models']
 
-def load_production_model_sklearn(model_type, ticker_symbol):
+
+def load_production_model_sklearn(model_type, ticker):
     """
     Loading the Sklearn models saved using the traditional Joblib format.
     """
-    MODELS_PATH = config['paths']['models_path']
-    model_file_path = f"{MODELS_PATH}/{model_type}/Model_{ticker_symbol}.joblib"
+    model_file_path = f"{MODELS_PATH}/{model_type}/{ticker}.joblib"
     current_prod_model = joblib.load(model_file_path)
+
     return current_prod_model
 
 
@@ -90,7 +102,7 @@ def create_future_frame(model_df: pd.DataFrame, forecast_horizon: int) -> pd.Dat
         freq='D'
     )
     future_df = pd.DataFrame({"DATE": future_dates})
-    future_df["STOCK"] = model_df["STOCK"].iloc[0]  # Use the first stock symbol from the model DataFrame
+    future_df[CATEGORY_COL] = model_df[CATEGORY_COL].iloc[0]  # Use the first stock symbol from the model DataFrame
 
     return future_df
 
@@ -118,15 +130,13 @@ def make_future_df(forecast_horzion: int, model_df: pd.DataFrame, features_list:
     Returns:
         future_df (pandas dataframe): The future dataframe used for forecasting.
     """
-    TARGET_NAME = config['model_config']['TARGET_NAME']
-
     future_df = create_future_frame(model_df, forecast_horzion)
     future_df = create_date_features(df=future_df)
     future_df = drop_weekends(future_df)
-    future_df = initialize_ma_values(model_df, features_list, TARGET_NAME, future_df)
-    future_df = initialize_lag_values(model_df, features_list, TARGET_NAME, future_df)
+    future_df = initialize_ma_values(model_df, features_list, TARGET_COL, future_df)
+    future_df = initialize_lag_values(model_df, features_list, TARGET_COL, future_df)
 
-    return future_df.reindex(columns=["DATE", "STOCK", *features_list])
+    return future_df.reindex(columns=["DATE", CATEGORY_COL, *features_list])
 
 
 def update_lag_features(future_df: pd.DataFrame, day: int, past_target_values: list, features: list) -> pd.DataFrame:
@@ -177,7 +187,7 @@ def make_iterative_predictions(model: Any, future_df: pd.DataFrame, past_target_
 
     for day in range(0, FH_WITHOUT_WEEKENDS):
 
-        X_inference = future_df_feat.drop(columns=["DATE", "STOCK"]).loc[[day]]
+        X_inference = future_df_feat.drop(columns=["DATE", CATEGORY_COL]).loc[[day]]
         prediction = model.predict(X_inference)[0]
 
         predictions.append(prediction)
@@ -187,14 +197,14 @@ def make_iterative_predictions(model: Any, future_df: pd.DataFrame, past_target_
             future_df_feat = update_lag_features(future_df_feat, day, past_target_values, all_features)
             future_df_feat = update_ma_features(future_df_feat, day, past_target_values, all_features)
     
-    future_df_feat["FORECAST"] = predictions
-    future_df_feat["FORECAST"] = future_df_feat["FORECAST"].astype('float64').round(2)
+    future_df_feat[PREDICTED_COL] = predictions
+    future_df_feat[PREDICTED_COL] = future_df_feat[PREDICTED_COL].astype('float64').round(2)
 
     return future_df_feat
 
 
 
-def inference_pipeline(model_type=None, ticker_symbol=None, write_to_table=True):
+def inference_pipeline(model_type=None, ticker=None, write_to_table=True):
     """
     Executes the model inference pipeline to generate stock price predictions.
 
@@ -212,7 +222,7 @@ def inference_pipeline(model_type=None, ticker_symbol=None, write_to_table=True)
 
     Args:
         model_type (str, optional): The type of model to use for inference. If None, all available models in the configuration will be used. Defaults to None.
-        ticker_symbol (str, optional): The ticker symbol of the stock to predict. If None, predictions are made for all stocks in the dataset. Defaults to None.
+        ticker (str, optional): The ticker symbol of the stock to predict. If None, predictions are made for all stocks in the dataset. Defaults to None.
         write_to_table (bool, optional): If True, the predictions will be written to a file or database table. Defaults to True.
 
     Returns:
@@ -221,47 +231,39 @@ def inference_pipeline(model_type=None, ticker_symbol=None, write_to_table=True)
     Raises:
         ValueError: If an invalid model type is provided.
     """
-
-    FORECAST_HORIZON = config['model_config']['forecast_horizon']
-    available_models = config['model_config']['available_models']
-    TARGET_NAME = config['model_config']['TARGET_NAME']
-    features_list = config['features_list']
-    PROCESSED_DATA_PATH = config['paths']['processed_data_path']
-    OUTPUT_DATA_PATH = config['paths']['output_data_path']
-
     logger.debug("Loading the featurized dataset...")
-    stock_df_feat_all = pd.read_csv(os.path.join(PROCESSED_DATA_PATH, 'processed_df.csv'), parse_dates=["DATE"])
-    
+    feature_df = pd.read_csv(os.path.join(PROCESSED_DATA_PATH, PROCESSED_DATA_NAME), parse_dates=["DATE"])
     final_predictions_df = pd.DataFrame()
+    available_models = model_config['available_models']
 
-    # Check the ticker_symbol parameter
-    if ticker_symbol:
-        ticker_symbol = ticker_symbol.upper() + '.SA'
-        stock_df_feat_all = stock_df_feat_all[stock_df_feat_all["STOCK"] == ticker_symbol]
+    # Check the ticker parameter
+    if ticker:
+        ticker = ticker.upper() + '.SA'
+        feature_df = feature_df[feature_df[CATEGORY_COL] == ticker]
 
     # Check the model_type parameter 
     if model_type is not None and model_type not in available_models:
         raise ValueError(f"Invalid model_type: {model_type}. Choose from: {available_models}")
     
     elif model_type:
-        available_models = [model_type.upper()]
+        available_models = [model_type]
 
-    for ticker_symbol in stock_df_feat_all["STOCK"].unique():
-        stock_df_feat = stock_df_feat_all[stock_df_feat_all["STOCK"] == ticker_symbol].copy()
+    for ticker in feature_df[CATEGORY_COL].unique():
+        filtered_feature_df = feature_df[feature_df[CATEGORY_COL] == ticker].copy()
 
         for model_type in available_models:
-            logger.debug(f"Performing inferece for ticker symbol [{ticker_symbol}] using model [{model_type}]...")
+            logger.debug(f"Performing inferece for ticker [{ticker}] using model [{model_type}]...")
  
-            current_prod_model = load_production_model_sklearn(model_type, ticker_symbol)
+            current_prod_model = load_production_model_sklearn(model_type, ticker)
 
             logger.debug("Creating the future dataframe...")
-            future_df = make_future_df(FORECAST_HORIZON, stock_df_feat, features_list)
+            future_df = make_future_df(FORECAST_HORIZON, filtered_feature_df, features_list)
 
             logger.debug("Predicting...")
             predictions_df = make_iterative_predictions(
                 model=current_prod_model,
                 future_df=future_df,
-                past_target_values=list(stock_df_feat[TARGET_NAME].values)
+                past_target_values=list(filtered_feature_df[TARGET_COL].values)
             )
             predictions_df['MODEL_TYPE'] = model_type
             final_predictions_df = pd.concat([final_predictions_df, predictions_df], axis=0)
@@ -273,7 +275,7 @@ def inference_pipeline(model_type=None, ticker_symbol=None, write_to_table=True)
     if write_to_table:
         logger.info("Writing the predictions to database...")
 
-        file_path = f"{OUTPUT_DATA_PATH}/forecast_output_df.csv"
+        file_path = f"{OUTPUT_DATA_PATH}/{OUTPUT_DATA_NAME}"
         if os.path.isfile(file_path):
             final_predictions_df.to_csv(file_path, mode='a', header=False, index=False)
         else:
@@ -292,22 +294,22 @@ if __name__ == "__main__":
     parser.add_argument(
         "-mt", "--model_type",
         type=str,
-        choices=["xgb", "et"],
-        help="Model name use for inference (xgb, et) (optional, defaults to all)."
+        choices=["XGB", "ET"],
+        help="Model name use for inference (XGB, ET) (optional, defaults to all)."
     )
     parser.add_argument(
-        "-ts", "--ticker_symbol",
+        "-ts", "--ticker",
         type=str,
         help="""Ticker Symbol for inference. (optional, defaults to all).
         Example: bova11 -> BOVA11.SA | petr4 -> PETR4.SA"""
     )
     parser.add_argument(
-        "-w", "--write_to_table",
+        "-dw", "--dont_write_to_table",
         action="store_false",
-        help="Disable Writing the OFS forecasts to table. Defaults to True. Run '--write_to_table' to Disable."
+        help="Disable Writing the OFS forecasts to table. Defaults to True. Run '--dont_write_to_table' to Disable."
     )
     args = parser.parse_args()
 
     logger.info("Starting the Inference pipeline...")
-    inference_pipeline(args.model_type.upper(), args.ticker_symbol, args.write_to_table)
+    inference_pipeline(args.model_type, args.ticker, args.dont_write_to_table)
     logger.info("Inference Pipeline completed successfully!")
